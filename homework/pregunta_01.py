3a9e55dddf60a46072d777b62408cf8894357b14
ValentinaON <vospinan@unal.edu.co>
2025-06-01 17:33:21 -0500

    Laboratorio terminado

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -3,6 +3,8 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
 
 # pylint: disable=import-outside-toplevel
+import pandas as pd
+import re
 
 
 def pregunta_01():
@@ -18,3 +20,56 @@ def pregunta_01():
 
 
     """
+    with open("files/input/clusters_report.txt", "r", encoding="utf-8") as file:
+        lines = file.readlines()
+
+    headers = [
+        "cluster",
+        "cantidad_de_palabras_clave",
+        "porcentaje_de_palabras_clave",
+        "principales_palabras_clave",
+    ]
+
+    data = []
+    current_row = []
+    for line in lines[4:]:
+
+        if re.match(r"^\s+\d+", line):
+            if current_row:
+                data.append(current_row)
+                current_row = []
+            current_row = re.split(r"\s{2,}", line.strip())
+            palabras_unidas = " ".join(current_row[3:])
+            current_row = current_row[0:3]
+            current_row.append(palabras_unidas)
+
+        else:
+            line = re.sub(r"\s{2,}", " ", line.strip())
+            current_row[-1] += " " + line.strip()
+
+    if current_row:
+        data.append(current_row)
+
+    df = pd.DataFrame(data, columns=headers)
+    df["cluster"] = df["cluster"].astype(int)
+    df["cantidad_de_palabras_clave"] = df["cantidad_de_palabras_clave"].astype(int)
+    df["porcentaje_de_palabras_clave"] = (
+        df["porcentaje_de_palabras_clave"]
+        .str.replace(",", ".")
+        .str.rstrip(" %")
+        .astype(float)
+    )
+    df["principales_palabras_clave"] = (
+        df["principales_palabras_clave"]
+        .str.replace(r"\s+", " ", regex=True)
+        .str.strip()
+    )
+    df["principales_palabras_clave"] = (
+        df["principales_palabras_clave"].str.replace(".", "").str.strip()
+    )
+
+    return df
+
+
+if "__main__" in __name__:
+    pregunta_01()
